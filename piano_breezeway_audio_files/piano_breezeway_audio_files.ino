709c10a9b9260e2bff6e9da8ba3b21e76dc0dfaf
Orlando Hoilett <orlandohoilett@pal-nat186-11-30.itap.purdue.edu>
2016-04-02 18:39:16 -0400

    updating files

--- piano_breezeway_audio_files/piano_breezeway_audio_files.ino
+++ piano_breezeway_audio_files/piano_breezeway_audio_files.ino
@@ -1,12 +1,6 @@
-String piano[] = {"pi40_C4.wav", "pi41.wav", "pi42.wav",
-  "pi43.wav", "pi44.wav", "pi45.wav", "pi46.wav", "pi47.wav",
-  "pi48.wav", "pi49.wav", "pi50.wav", "pi51.wav", "pi52_C5.wav",
-  "pi53.wav", "pi54.wav", "pi55.wav", "pi56.wav", "pi57.wav",
-  "pi58.wav", "pi59.wav", "pi60.wav", "pi61.wav", "pi62.wav",
-  "pi63.wav", "pi64_C6.wav" };
+const uint8_t numKeys = 25;
 
-
-String cello[] = {"Ce1_C2.wav", "Ce2_Db2.wav", "Ce3_D2.wav",
+String cello[numKeys] = {"Ce1_C2.wav", "Ce2_Db2.wav", "Ce3_D2.wav",
   "Ce4_Eb2.wav", "Ce5_E2.wav", "Ce6_F2.wav", "Ce7_Gb2.wav",
   "Ce8_G2.wav", "Ce9_Ab2.wav", "Ce10_A2.wav", "Ce11_Bb2.wav",
   "Ce12_B2.wav", "Ce13_C3.wav", "Ce14_Db3.wav", "Ce15_D3.wav",
@@ -14,6 +8,54 @@ String cello[] = {"Ce1_C2.wav", "Ce2_Db2.wav", "Ce3_D2.wav",
   "Ce20_G3.wav", "Ce21_Ab3.wav", "Ce22_A3.wav", "Ce23_Bb3.wav",
   "Ce24_B3.wav", "Ce25_C4.wav"};
 
+
+String clarinet[numKeys] = {"Cl1_C4.wav", "Cl2_Db4.wav", "Cl3_D4.wav",
+  "Cl4_Eb4.wav", "Cl5_E4.wav", "Cl6_F4.wav", "Cl7_Gb4.wav",
+  "Cl8_G4.wav", "Cl9_Ab4.wav", "Cl10_A4.wav", "Cl11_Bb4.wav",
+  "Cl12_B4.wav", "Cl13_C5.wav", "Cl14_Db5.wav", "Cl15_D5.wav",
+  "Cl16_Eb5.wav", "Cl17_E5.wav", "Cl18_F5.wav", "Cl19_Gb5.wav",
+  "Cl20_G5.wav", "Cl21_Ab5.wav", "Cl22_A5.wav", "Cl23_Bb5.wav",
+  "Cl24_B5.wav", "Cl25_C6.wav"};
+
+
+String guitar[numKeys] = {"Gu1_C1.wav", "Gu2_Db1.wav",
+  "Gu3_D1.wav", "Gu4_Eb1.wav", "Gu5_E1.wav", "Gu6_F1.wav",
+  "Gu7_Gb1.wav", "Gu8_G1.wav", "Gu9_Ab1.wav", "Gu10_A1.wav",
+  "Gu11_Bb1.wav", "Gu12_B1.wav", "Gu13_C2.wav", "Gu14_Db2.wav",
+  "Gu15_D2.wav", "Gu16_Eb2.wav", "Gu17_E2.wav", "Gu18_F2.wav",
+  "Gu19_Gb2.wav", "Gu20_G2.wav", "Gu21_Ab2.wav", "Gu22_A2.wav",
+  "Gu23_Bb2.wav", "Gu24_B2.wav", "Gu25_C3.wav" };
+
+String piano[numKeys] = {"pi40_C4.wav", "pi41.wav", "pi42.wav",
+  "pi43.wav", "pi44.wav", "pi45.wav", "pi46.wav", "pi47.wav",
+  "pi48.wav", "pi49.wav", "pi50.wav", "pi51.wav", "pi52_C5.wav",
+  "pi53.wav", "pi54.wav", "pi55.wav", "pi56.wav", "pi57.wav",
+  "pi58.wav", "pi59.wav", "pi60.wav", "pi61.wav", "pi62.wav",
+  "pi63.wav", "pi64_C6.wav" };
+
+String sexyphone[numKeys] = {"Sa1_C3.wav", "Sa2_Db3.wav",
+  "Sa3_D3.wav", "Sa4_Eb3.wav", "Sa5_E3.wav", "Sa6_F3.wav",
+  "Sa7_Gb3.wav", "Sa8_G3.wav", "Sa9_Ab3.wav", "Sa10_A3.wav",
+  "Sa11_Bb3.wav", "Sa12_B3.wav", "Sa13_C4.wav", "Sa14_Db4.wav",
+  "Sa15_D4.wav", "Sa16_Eb4.wav", "Sa17_E4.wav", "Sa18_F4.wav",
+  "Sa19_Gb4.wav", "Sa20_G4.wav", "Sa21_Ab4.wav", "Sa22_A4.wav",
+  "Sa23_Bb4.wav", "Sa24_B4.wav", "Sa25_C5.wav" };
+
+String steelDrum[numKeys] = {"SD1_C1.wav", "SD2_Db1.wav",
+  "SD3_D1.wav", "SD4_Eb1.wav", "SD5_E1.wav", "SD6_F1.wav",
+  "SD7_Gb1.wav", "SD8_G1.wav", "SD9_Ab1.wav", "SD10_A1.wav",
+  "SD11_Bb1.wav", "SD12_B1.wav", "SD13_C2.wav", "SD14_Db2.wav",
+  "SD15_D2.wav", "SD16_Eb2.wav", "SD17_E2.wav", "SD18_F2.wav",
+  "SD19_Gb2.wav", "SD20_G2.wav", "SD21_Ab2.wav", "SD22_A2.wav",
+  "SD23_Bb2.wav", "SD24_B2.wav", "SD25_C3.wav" };
+
+String xylophone[numKeys] = {"Xy1_C5.wav", "Xy2_Db5.wav",
+  "Xy3_D5.wav", "Xy4_Eb5.wav", "Xy5_E5.wav", "Xy6_F5.wav",
+  "Xy7_Gb5.wav", "Xy8_G5.wav", "Xy9_Ab5.wav", "Xy10_A5.wav",
+  "Xy11_Bb5.wav", "Xy12_B5.wav", "Xy13_C6.wav", "Xy14_Db5.wav",
+  "Xy15_D6.wav", "Xy16_Eb6.wav", "Xy17_E6.wav", "Xy18_F6.wav",
+  "Xy19_Gb6.wav","Xy20_G6.wav", "Xy21_Ab6.wav", "Xy22_A6.wav",
+  "Xy23_Bb6.wav", "Xy24_B6.wav", "Xy25_C7.wav" };
 
 
 void setup() {
---
+++ testing_playing_music_with_all_notes/testing_playing_music_with_all_notes.ino
@@ -0,0 +1,183 @@
+/*************************************************** 
+  This is an example for the Adafruit VS1053 Codec Breakout
+
+  Designed specifically to work with the Adafruit VS1053 Codec Breakout 
+  ----> https://www.adafruit.com/products/1381
+
+  Adafrit invests time and resources providing this open source code, 
+  please support Adafruit and open-source hardware by purchasing 
+  products from Adafruit!
+
+  Written by Limor Fried/Ladyada for Adafruit Industries.  
+  BSD license, all text above must be included in any redistribution
+ ****************************************************/
+
+//const int numKeys = 25;
+//
+//String piano[numKeys] = {"pi40_C4.mp3", "pi41.mp3", "pi42.mp3",
+//  "pi43.mp3", "pi44.mp3", "pi45.mp3", "pi46.mp3", "pi47.mp3",
+//  "pi48.mp3", "pi49.mp3", "pi50.mp3", "pi51.mp3", "pi52_C5.mp3",
+//  "pi53.mp3", "pi54.mp3", "pi55.mp3", "pi56.mp3", "pi57.mp3",
+//  "pi58.mp3", "pi59.mp3", "pi60.mp3", "pi61.mp3", "pi62.mp3",
+//  "pi63.mp3", "pi64_C6.mp3" };
+
+
+const uint8_t numKeys = 25;
+
+//not okay
+String cello[numKeys] = {"Ce1_C2.mp3", "Ce2_Db2.mp3", "Ce3_D2.mp3",
+  "Ce4_Eb2.mp3", "Ce5_E2.mp3", "Ce6_F2.mp3", "Ce7_Gb2.mp3",
+  "Ce8_G2.mp3", "Ce9_Ab2.mp3", "Ce10_A2.mp3", "Ce11_Bb2.mp3",
+  "Ce12_B2.mp3", "Ce13_C3.mp3", "Ce14_Db3.mp3", "Ce15_D3.mp3",
+  "Ce16_Eb3.mp3", "Ce17_E3.mp3", "Ce18_F3.mp3", "Ce19_Gb3.mp3",
+  "Ce20_G3.mp3", "Ce21_Ab3.mp3", "Ce22_A3.mp3", "Ce23_Bb3.mp3",
+  "Ce24_B3.mp3", "Ce25_C4.mp3"};
+
+//not okay
+String clarinet[numKeys] = {"Cl1_C4.mp3", "Cl2_Db4.mp3", "Cl3_D4.mp3",
+  "Cl4_Eb4.mp3", "Cl5_E4.mp3", "Cl6_F4.mp3", "Cl7_Gb4.mp3",
+  "Cl8_G4.mp3", "Cl9_Ab4.mp3", "Cl10_A4.mp3", "Cl11_Bb4.mp3",
+  "Cl12_B4.mp3", "Cl13_C5.mp3", "Cl14_Db5.mp3", "Cl15_D5.mp3",
+  "Cl16_Eb5.mp3", "Cl17_E5.mp3", "Cl18_F5.mp3", "Cl19_Gb5.mp3",
+  "Cl20_G5.mp3", "Cl21_Ab5.mp3", "Cl22_A5.mp3", "Cl23_Bb5.mp3",
+  "Cl24_B5.mp3", "Cl25_C6.mp3"};
+
+//fine
+String guitar[numKeys] = {"Gu1_C1.mp3", "Gu2_Db1.mp3",
+  "Gu3_D1.mp3", "Gu4_Eb1.mp3", "Gu5_E1.mp3", "Gu6_F1.mp3",
+  "Gu7_Gb1.mp3", "Gu8_G1.mp3", "Gu9_Ab1.mp3", "Gu10_A1.mp3",
+  "Gu11_Bb1.mp3", "Gu12_B1.mp3", "Gu13_C2.mp3", "Gu14_Db2.mp3",
+  "Gu15_D2.mp3", "Gu16_Eb2.mp3", "Gu17_E2.mp3", "Gu18_F2.mp3",
+  "Gu19_Gb2.mp3", "Gu20_G2.mp3", "Gu21_Ab2.mp3", "Gu22_A2.mp3",
+  "Gu23_Bb2.mp3", "Gu24_B2.mp3", "Gu25_C3.mp3" };
+
+//great
+String piano[numKeys] = {"pi40_C4.mp3", "pi41.mp3", "pi42.mp3",
+  "pi43.mp3", "pi44.mp3", "pi45.mp3", "pi46.mp3", "pi47.mp3",
+  "pi48.mp3", "pi49.mp3", "pi50.mp3", "pi51.mp3", "pi52_C5.mp3",
+  "pi53.mp3", "pi54.mp3", "pi55.mp3", "pi56.mp3", "pi57.mp3",
+  "pi58.mp3", "pi59.mp3", "pi60.mp3", "pi61.mp3", "pi62.mp3",
+  "pi63.mp3", "pi64_C6.mp3" };
+
+//not okay
+String sexyphone[numKeys] = {"Sa1_C3.mp3", "Sa2_Db3.mp3",
+  "Sa3_D3.mp3", "Sa4_Eb3.mp3", "Sa5_E3.mp3", "Sa6_F3.mp3",
+  "Sa7_Gb3.mp3", "Sa8_G3.mp3", "Sa9_Ab3.mp3", "Sa10_A3.mp3",
+  "Sa11_Bb3.mp3", "Sa12_B3.mp3", "Sa13_C4.mp3", "Sa14_Db4.mp3",
+  "Sa15_D4.mp3", "Sa16_Eb4.mp3", "Sa17_E4.mp3", "Sa18_F4.mp3",
+  "Sa19_Gb4.mp3", "Sa20_G4.mp3", "Sa21_Ab4.mp3", "Sa22_A4.mp3",
+  "Sa23_Bb4.mp3", "Sa24_B4.mp3", "Sa25_C5.mp3" };
+
+//one note is off
+String steelDrum[numKeys] = {"SD1_C1.mp3", "SD2_Db1.mp3",
+  "SD3_D1.mp3", "SD4_Eb1.mp3", "SD5_E1.mp3", "SD6_F1.mp3",
+  "SD7_Gb1.mp3", "SD8_G1.mp3", "SD9_Ab1.mp3", "SD10_A1.mp3",
+  "SD11_Bb1.mp3", "SD12_B1.mp3", "SD13_C2.mp3", "SD14_Db2.mp3",
+  "SD15_D2.mp3", "SD16_Eb2.mp3", "SD17_E2.mp3", "SD18_F2.mp3",
+  "SD19_Gb2.mp3", "SD20_G2.mp3", "SD21_Ab2.mp3", "SD22_A2.mp3",
+  "SD23_Bb2.mp3", "SD24_B2.mp3", "SD25_C3.mp3" };
+
+//not okay
+String xylophone[numKeys] = {"Xy1_C5.mp3", "Xy2_Db5.mp3",
+  "Xy3_D5.mp3", "Xy4_Eb5.mp3", "Xy5_E5.mp3", "Xy6_F5.mp3",
+  "Xy7_Gb5.mp3", "Xy8_G5.mp3", "Xy9_Ab5.mp3", "Xy10_A5.mp3",
+  "Xy11_Bb5.mp3", "Xy12_B5.mp3", "Xy13_C6.mp3", "Xy14_Db5.mp3",
+  "Xy15_D6.mp3", "Xy16_Eb6.mp3", "Xy17_E6.mp3", "Xy18_F6.mp3",
+  "Xy19_Gb6.mp3","Xy20_G6.mp3", "Xy21_Ab6.mp3", "Xy22_A6.mp3",
+  "Xy23_Bb6.mp3", "Xy24_B6.mp3", "Xy25_C7.mp3" };
+
+
+
+// include SPI, MP3 and SD libraries
+#include <SPI.h>
+#include <Adafruit_VS1053.h>
+#include <SD.h>
+
+// define the pins used
+#define CLK 13       // SPI Clock, shared with SD card
+#define MISO 12      // Input data, from VS1053/SD card
+#define MOSI 11      // Output data, to VS1053/SD card
+// Connect CLK, MISO and MOSI to hardware SPI pins. 
+// See http://arduino.cc/en/Reference/SPI "Connections"
+
+// These are the pins used for the breakout example
+#define BREAKOUT_RESET  9      // VS1053 reset pin (output)
+#define BREAKOUT_CS     10     // VS1053 chip select pin (output)
+#define BREAKOUT_DCS    8      // VS1053 Data/command select pin (output)
+// These are the pins used for the music maker shield
+#define SHIELD_RESET  -1      // VS1053 reset pin (unused!)
+#define SHIELD_CS     7      // VS1053 chip select pin (output)
+#define SHIELD_DCS    6      // VS1053 Data/command select pin (output)
+
+// These are common pins between breakout and shield
+#define CARDCS 4     // Card chip select pin
+// DREQ should be an Int pin, see http://arduino.cc/en/Reference/attachInterrupt
+#define DREQ 3       // VS1053 Data request, ideally an Interrupt pin
+
+Adafruit_VS1053_FilePlayer musicPlayer = 
+  // create breakout-example object!
+  //Adafruit_VS1053_FilePlayer(BREAKOUT_RESET, BREAKOUT_CS, BREAKOUT_DCS, DREQ, CARDCS);
+  // create shield-example object!
+  Adafruit_VS1053_FilePlayer(SHIELD_RESET, SHIELD_CS, SHIELD_DCS, DREQ, CARDCS);
+  
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Adafruit VS1053 Simple Test");
+
+  if (! musicPlayer.begin()) { // initialise the music player
+     Serial.println(F("Couldn't find VS1053, do you have the right pins defined?"));
+     while (1);
+  }
+  Serial.println(F("VS1053 found"));
+  
+  SD.begin(CARDCS);    // initialise the SD card
+  
+  // Set volume for left, right channels. lower numbers == louder volume!
+  musicPlayer.setVolume(5,5);
+
+  // Timer interrupts are not suggested, better to use DREQ interrupt!
+  //musicPlayer.useInterrupt(VS1053_FILEPLAYER_TIMER0_INT); // timer int
+
+  // If DREQ is on an interrupt pin (on uno, #2 or #3) we can do background
+  // audio playing
+  musicPlayer.useInterrupt(VS1053_FILEPLAYER_PIN_INT);  // DREQ int
+  
+  Serial.println(F("Playing track 001"));
+  for (int i = 0; i < numKeys; i++)
+  {
+    char filename[12];
+    cello[i].toCharArray(filename, 12);
+    musicPlayer.startPlayingFile(filename);
+    //musicPlayer.playFullFile(filename);
+    delay(2000);
+  }
+}
+
+void loop() {
+  // File is playing in the background
+  if (musicPlayer.stopped()) {
+    Serial.println("Done playing music");
+    while (1);
+  }
+  if (Serial.available()) {
+    char c = Serial.read();
+    
+    // if we get an 's' on the serial console, stop!
+    if (c == 's') {
+      musicPlayer.stopPlaying();
+    }
+    
+    // if we get an 'p' on the serial console, pause/unpause!
+    if (c == 'p') {
+      if (! musicPlayer.paused()) {
+        Serial.println("Paused");
+        musicPlayer.pausePlaying(true);
+      } else { 
+        Serial.println("Resumed");
+        musicPlayer.pausePlaying(false);
+      }
+    }
+  }
+
+  delay(100);
+}
