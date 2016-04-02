6d21d94bdbe5f58957e443b66a3afd9bce37fc5c
Orlando Hoilett <orlandohoilett@pal-nat186-11-30.itap.purdue.edu>
2016-04-02 17:32:23 -0400

    naming audio files to string arrays

---
+++ piano_breezeway_audio_files/piano_breezeway_audio_files.ino
@@ -0,0 +1,27 @@
+String piano[] = {"pi40_C4.wav", "pi41.wav", "pi42.wav",
+  "pi43.wav", "pi44.wav", "pi45.wav", "pi46.wav", "pi47.wav",
+  "pi48.wav", "pi49.wav", "pi50.wav", "pi51.wav", "pi52_C5.wav",
+  "pi53.wav", "pi54.wav", "pi55.wav", "pi56.wav", "pi57.wav",
+  "pi58.wav", "pi59.wav", "pi60.wav", "pi61.wav", "pi62.wav",
+  "pi63.wav", "pi64_C6.wav" };
+
+
+String cello[] = {"Ce1_C2.wav", "Ce2_Db2.wav", "Ce3_D2.wav",
+  "Ce4_Eb2.wav", "Ce5_E2.wav", "Ce6_F2.wav", "Ce7_Gb2.wav",
+  "Ce8_G2.wav", "Ce9_Ab2.wav", "Ce10_A2.wav", "Ce11_Bb2.wav",
+  "Ce12_B2.wav", "Ce13_C3.wav", "Ce14_Db3.wav", "Ce15_D3.wav",
+  "Ce16_Eb3.wav", "Ce17_E3.wav", "Ce18_F3.wav", "Ce19_Gb3.wav",
+  "Ce20_G3.wav", "Ce21_Ab3.wav", "Ce22_A3.wav", "Ce23_Bb3.wav",
+  "Ce24_B3.wav", "Ce25_C4.wav"};
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
