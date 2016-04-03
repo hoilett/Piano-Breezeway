3c00c2060de32cbc4a2aa8d479141fa8630cfc48
Orlando Hoilett <orlandohoilett@pal-nat186-2-75.itap.purdue.edu>
2016-04-02 22:36:52 -0400

    making updates

--- Piano_Staircase/Piano_Staircase.ino
+++ Piano_Staircase/Piano_Staircase.ino
@@ -1,3 +1,69 @@
+const uint8_t numKeys = 25;
+
+//not okay
+String cello[numKeys] = {"Ce1_C2.mp3", "Ce2_Db2.mp3", "Ce3_D2.mp3",
+  "Ce4_Eb2.mp3", "Ce5_E2.mp3", "Ce6_F2.mp3", "Ce7_Gb2.mp3",
+  "Ce8_G2.mp3", "Ce9_Ab2.mp3", "Ce10_A2.mp3", "Ce11_Bb2.mp3",
+  "Ce12_B2.mp3", "Ce13_C3.mp3", "Ce14_Db3.mp3", "Ce15_D3.mp3",
+  "Ce16_Eb3.mp3", "Ce17_E3.mp3", "Ce18_F3.mp3", "Ce19_Gb3.mp3",
+  "Ce20_G3.mp3", "Ce21_Ab3.mp3", "Ce22_A3.mp3", "Ce23_Bb3.mp3",
+  "Ce24_B3.mp3", "Ce25_C4.mp3"};
+
+//not okay
+String clarinet[numKeys] = {"Cl1_C4.mp3", "Cl2_Db4.mp3", "Cl3_D4.mp3",
+  "Cl4_Eb4.mp3", "Cl5_E4.mp3", "Cl6_F4.mp3", "Cl7_Gb4.mp3",
+  "Cl8_G4.mp3", "Cl9_Ab4.mp3", "Cl10_A4.mp3", "Cl11_Bb4.mp3",
+  "Cl12_B4.mp3", "Cl13_C5.mp3", "Cl14_Db5.mp3", "Cl15_D5.mp3",
+  "Cl16_Eb5.mp3", "Cl17_E5.mp3", "Cl18_F5.mp3", "Cl19_Gb5.mp3",
+  "Cl20_G5.mp3", "Cl21_Ab5.mp3", "Cl22_A5.mp3", "Cl23_Bb5.mp3",
+  "Cl24_B5.mp3", "Cl25_C6.mp3"};
+
+//good
+String guitar[numKeys] = {"Gu1_C1.mp3", "Gu2_Db1.mp3",
+  "Gu3_D1.mp3", "Gu4_Eb1.mp3", "Gu5_E1.mp3", "Gu6_F1.mp3",
+  "Gu7_Gb1.mp3", "Gu8_G1.mp3", "Gu9_Ab1.mp3", "Gu10_A1.mp3",
+  "Gu11_Bb1.mp3", "Gu12_B1.mp3", "Gu13_C2.mp3", "Gu14_Db2.mp3",
+  "Gu15_D2.mp3", "Gu16_Eb2.mp3", "Gu17_E2.mp3", "Gu18_F2.mp3",
+  "Gu19_Gb2.mp3", "Gu20_G2.mp3", "Gu21_Ab2.mp3", "Gu22_A2.mp3",
+  "Gu23_Bb2.mp3", "Gu24_B2.mp3", "Gu25_C3.mp3" };
+
+//sounds great!
+String piano[numKeys] = {"pi40_C4.mp3", "pi41.mp3", "pi42.mp3",
+  "pi43.mp3", "pi44.mp3", "pi45.mp3", "pi46.mp3", "pi47.mp3",
+  "pi48.mp3", "pi49.mp3", "pi50.mp3", "pi51.mp3", "pi52_C5.mp3",
+  "pi53.mp3", "pi54.mp3", "pi55.mp3", "pi56.mp3", "pi57.mp3",
+  "pi58.mp3", "pi59.mp3", "pi60.mp3", "pi61.mp3", "pi62.mp3",
+  "pi63.mp3", "pi64_C6.mp3" };
+
+//fine
+String sexyphone[numKeys] = {"Sa1_C3.mp3", "Sa2_Db3.mp3",
+  "Sa3_D3.mp3", "Sa4_Eb3.mp3", "Sa5_E3.mp3", "Sa6_F3.mp3",
+  "Sa7_Gb3.mp3", "Sa8_G3.mp3", "Sa9_Ab3.mp3", "Sa10_A3.mp3",
+  "Sa11_Bb3.mp3", "Sa12_B3.mp3", "Sa13_C4.mp3", "Sa14_Db4.mp3",
+  "Sa15_D4.mp3", "Sa16_Eb4.mp3", "Sa17_E4.mp3", "Sa18_F4.mp3",
+  "Sa19_Gb4.mp3", "Sa20_G4.mp3", "Sa21_Ab4.mp3", "Sa22_A4.mp3",
+  "Sa23_Bb4.mp3", "Sa24_B4.mp3", "Sa25_C5.mp3" };
+
+//good - maybe two or three come in a little late
+String steelDrum[numKeys] = {"SD1_C1.mp3", "SD2_Db1.mp3",
+  "SD3_D1.mp3", "SD4_Eb1.mp3", "SD5_E1.mp3", "SD6_F1.mp3",
+  "SD7_Gb1.mp3", "SD8_G1.mp3", "SD9_Ab1.mp3", "SD10_A1.mp3",
+  "SD11_Bb1.mp3", "SD12_B1.mp3", "SD13_C2.mp3", "SD14_Db2.mp3",
+  "SD15_D2.mp3", "SD16_Eb2.mp3", "SD17_E2.mp3", "SD18_F2.mp3",
+  "SD19_Gb2.mp3", "SD20_G2.mp3", "SD21_Ab2.mp3", "SD22_A2.mp3",
+  "SD23_Bb2.mp3", "SD24_B2.mp3", "SD25_C3.mp3" };
+
+//good - one off Db5
+String xylophone[numKeys] = {"Xy1_C5.mp3", "Xy2_Db5.mp3",
+  "Xy3_D5.mp3", "Xy4_Eb5.mp3", "Xy5_E5.mp3", "Xy6_F5.mp3",
+  "Xy7_Gb5.mp3", "Xy8_G5.mp3", "Xy9_Ab5.mp3", "Xy10_A5.mp3",
+  "Xy11_Bb5.mp3", "Xy12_B5.mp3", "Xy13_C6.mp3", "Xy14_Db5.mp3",
+  "Xy15_D6.mp3", "Xy16_Eb6.mp3", "Xy17_E6.mp3", "Xy18_F6.mp3",
+  "Xy19_Gb6.mp3","Xy20_G6.mp3", "Xy21_Ab6.mp3", "Xy22_A6.mp3",
+  "Xy23_Bb6.mp3", "Xy24_B6.mp3", "Xy25_C7.mp3" };
+
+
+
 // include SPI, MP3 and SD libraries
 #include <SPI.h>
 #include <Adafruit_VS1053.h>
@@ -5,9 +71,9 @@
 
 
 // define the pins used
-//#define CLK 13       // SPI Clock, shared with SD card
-//#define MISO 12      // Input data, from VS1053/SD card
-//#define MOSI 11      // Output data, to VS1053/SD card
+#define CLK 13       // SPI Clock, shared with SD card
+#define MISO 12      // Input data, from VS1053/SD card
+#define MOSI 11      // Output data, to VS1053/SD card
 // Connect CLK, MISO and MOSI to hardware SPI pins. 
 // See http://arduino.cc/en/Reference/SPI "Connections"
 
@@ -24,6 +90,8 @@
 Adafruit_VS1053_FilePlayer musicPlayer =
     Adafruit_VS1053_FilePlayer(SHIELD_RESET, SHIELD_CS, SHIELD_DCS, DREQ, CARDCS);
 
+char filename[14];
+int i = 0;
 
 
 //Arduino IO pins
--- testing_playing_music_with_all_notes/testing_playing_music_with_all_notes.ino
+++ testing_playing_music_with_all_notes/testing_playing_music_with_all_notes.ino
@@ -24,7 +24,7 @@
 
 const uint8_t numKeys = 25;
 
-//not okay
+//play every 75ms
 String cello[numKeys] = {"Ce1_C2.mp3", "Ce2_Db2.mp3", "Ce3_D2.mp3",
   "Ce4_Eb2.mp3", "Ce5_E2.mp3", "Ce6_F2.mp3", "Ce7_Gb2.mp3",
   "Ce8_G2.mp3", "Ce9_Ab2.mp3", "Ce10_A2.mp3", "Ce11_Bb2.mp3",
@@ -42,7 +42,7 @@ String clarinet[numKeys] = {"Cl1_C4.mp3", "Cl2_Db4.mp3", "Cl3_D4.mp3",
   "Cl20_G5.mp3", "Cl21_Ab5.mp3", "Cl22_A5.mp3", "Cl23_Bb5.mp3",
   "Cl24_B5.mp3", "Cl25_C6.mp3"};
 
-//good
+//150ms
 String guitar[numKeys] = {"Gu1_C1.mp3", "Gu2_Db1.mp3",
   "Gu3_D1.mp3", "Gu4_Eb1.mp3", "Gu5_E1.mp3", "Gu6_F1.mp3",
   "Gu7_Gb1.mp3", "Gu8_G1.mp3", "Gu9_Ab1.mp3", "Gu10_A1.mp3",
@@ -51,6 +51,7 @@ String guitar[numKeys] = {"Gu1_C1.mp3", "Gu2_Db1.mp3",
   "Gu19_Gb2.mp3", "Gu20_G2.mp3", "Gu21_Ab2.mp3", "Gu22_A2.mp3",
   "Gu23_Bb2.mp3", "Gu24_B2.mp3", "Gu25_C3.mp3" };
 
+//20ms
 //sounds great!
 String piano[numKeys] = {"pi40_C4.mp3", "pi41.mp3", "pi42.mp3",
   "pi43.mp3", "pi44.mp3", "pi45.mp3", "pi46.mp3", "pi47.mp3",
@@ -59,7 +60,7 @@ String piano[numKeys] = {"pi40_C4.mp3", "pi41.mp3", "pi42.mp3",
   "pi58.mp3", "pi59.mp3", "pi60.mp3", "pi61.mp3", "pi62.mp3",
   "pi63.mp3", "pi64_C6.mp3" };
 
-//fine
+//200ms
 String sexyphone[numKeys] = {"Sa1_C3.mp3", "Sa2_Db3.mp3",
   "Sa3_D3.mp3", "Sa4_Eb3.mp3", "Sa5_E3.mp3", "Sa6_F3.mp3",
   "Sa7_Gb3.mp3", "Sa8_G3.mp3", "Sa9_Ab3.mp3", "Sa10_A3.mp3",
@@ -68,7 +69,7 @@ String sexyphone[numKeys] = {"Sa1_C3.mp3", "Sa2_Db3.mp3",
   "Sa19_Gb4.mp3", "Sa20_G4.mp3", "Sa21_Ab4.mp3", "Sa22_A4.mp3",
   "Sa23_Bb4.mp3", "Sa24_B4.mp3", "Sa25_C5.mp3" };
 
-//good - maybe two or three come in a little late
+//150ms
 String steelDrum[numKeys] = {"SD1_C1.mp3", "SD2_Db1.mp3",
   "SD3_D1.mp3", "SD4_Eb1.mp3", "SD5_E1.mp3", "SD6_F1.mp3",
   "SD7_Gb1.mp3", "SD8_G1.mp3", "SD9_Ab1.mp3", "SD10_A1.mp3",
@@ -77,7 +78,7 @@ String steelDrum[numKeys] = {"SD1_C1.mp3", "SD2_Db1.mp3",
   "SD19_Gb2.mp3", "SD20_G2.mp3", "SD21_Ab2.mp3", "SD22_A2.mp3",
   "SD23_Bb2.mp3", "SD24_B2.mp3", "SD25_C3.mp3" };
 
-//good - one off Db5
+//50ms
 String xylophone[numKeys] = {"Xy1_C5.mp3", "Xy2_Db5.mp3",
   "Xy3_D5.mp3", "Xy4_Eb5.mp3", "Xy5_E5.mp3", "Xy6_F5.mp3",
   "Xy7_Gb5.mp3", "Xy8_G5.mp3", "Xy9_Ab5.mp3", "Xy10_A5.mp3",
@@ -160,21 +161,23 @@ void setup() {
 //    //delay(300);
 //    musicPlayer.stopPlaying();
 //  }
+
+  //musicPlayer.startPlayingFile("Xy16_Eb6.mp3");
 }
 
 void loop()
 {
-  for (int i = 0; i < 13; i++)
+  for (int i = 0; i < 15; i++)
   {
     filename[i] = 0;
   }
   
-  cello[i].toCharArray(filename, 14);
+  piano[i].toCharArray(filename, 15);
   musicPlayer.startPlayingFile(filename);
-  Serial.println(filename);
+  //Serial.println(filename);
 
   unsigned long startTime = millis();
-  while ((millis()-startTime) < 200);
+  while ((millis()-startTime) < 100);
   
   musicPlayer.stopPlaying();
 
